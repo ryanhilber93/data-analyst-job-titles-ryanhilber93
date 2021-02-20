2f4f50a06f7a67dfa61798de8f78df9968e06e97
ryanhilber93 <ryanhilber93@hotmail.com>
2021-02-20 14:00:19 -0600

    Final submission

--- data/jobs_project.sql
+++ data/jobs_project.sql
@@ -1,39 +1,77 @@
--- #1 SELECT COUNT(*) FROM data_analyst_jobs; 1793 rows
--- #2 SELECT * FROM data_analyst_jobs LIMIT 10; ExxonMobil
--- #3a SELECT COUNT(*) FROM data_analyst_jobs WHERE location = 'TN'; 21
--- #3b SELECT COUNT(*) FROM data_analyst_jobs WHERE location = 'TN' OR location ='KY'; 27
-/* #4 SELECT COUNT(*) FROM data_analyst_jobs 
+-- #1 
+SELECT COUNT(*) FROM data_analyst_jobs; 
+-- ans: 1793 rows 
+-- #2  
+SELECT * FROM data_analyst_jobs LIMIT 10; 
+-- ans: ExxonMobil */ 
+-- #3a 
+SELECT COUNT(*) FROM data_analyst_jobs WHERE location = 'TN'; 
+-- ans: 21 
+-- #3b 
+SELECT COUNT(*) FROM data_analyst_jobs WHERE location = 'TN' OR location ='KY';
+-- ans: 27 
+-- could have used WHERE location IN ('TN', 'KY') b/c then could easily add more states
+-- #4 
+SELECT COUNT(*) FROM data_analyst_jobs 
 WHERE location = 'TN'
-AND star_rating > 4; 3 */
-/* #5 SELECT COUNT(*) FROM data_analyst_jobs 
-WHERE review_count > 500 AND review_count < 1000; 150 */
-/* #6 SELECT location AS state, AVG(star_rating) AS avg_rating 
+AND star_rating > 4;
+-- ans: 3 
+-- #5 
+SELECT COUNT(*) FROM data_analyst_jobs 
+WHERE review_count BETWEEN 500 AND 1000
+-- ans: 151
+-- #6 
+SELECT location AS state, ROUND(AVG(star_rating),2) AS avg_rating 
 FROM data_analyst_jobs
+WHERE location IS NOT null AND star_rating IS NOT null
 GROUP BY state
-ORDER BY avg_rating DESC; NE */
-/* #7 SELECT DISTINCT title FROM data_analyst_jobs; 881 */
-/* #8 SELECT DISTINCT title FROM data_analyst_jobs WHERE location = 'CA'; 230 */
-/* #9 SELECT company AS name, AVG(star_rating) AS avg_rating
+ORDER BY avg_rating DESC; 
+-- or
+SELECT location as state, ROUND(AVG(star_rating), 2) as avg_stars
+FROM (SELECT DISTINCT company, location, star_rating
+	FROM data_analyst_jobs
+	WHERE location IS NOT NULL and star_rating IS NOT NULL) sub
+GROUP BY location
+ORDER BY avg_stars DESC;
+-- second way accounts for companies with more than one entry per state
+--ans: NE 
+-- #7 
+SELECT COUNT(DISTINCT title) FROM data_analyst_jobs; 
+-- ans: 881 
+-- #8 
+SELECT COUNT(DISTINCT title) FROM data_analyst_jobs WHERE location = 'CA'; 
+-- ans: 230 
+-- #9 
+SELECT company AS name, ROUND(AVG(star_rating), 2) AS avg_rating
 FROM data_analyst_jobs
-WHERE review_count > 5000
-GROUP BY name; 41 - should there be null results? */
-/* #10 SELECT company AS name, AVG(star_rating) AS avg_rating
+WHERE review_count > 5000 AND company IS NOT null
+GROUP BY name; 
+-- ans: 40  
+-- #10 
+SELECT company AS name, ROUND(AVG(star_rating), 2) AS avg_rating
 FROM data_analyst_jobs
 WHERE review_count > 5000
 GROUP BY name
-ORDER BY avg_rating DESC; General Motors 4.2, top 5 are exactly the same? */
-/* #11 SELECT DISTINCT title 
+ORDER BY avg_rating DESC; 
+-- ans: General Motors 4.2, top 6 are exactly the same? 
+-- #11 
+SELECT DISTINCT title 
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%'; 754 */
-/* #12 SELECT DISTINCT title
+WHERE title ILIKE '%Analyst%'; 
+-- ans: if case sensitive - 754 
+-- ans: if not case sesitive - 774
+-- #12 
+SELECT DISTINCT title
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%' AND title NOT LIKE '%Analytics%'; 
-26 They mostly have those words just in all lowercase or all caps */
-/* Bonus SELECT domain, COUNT(domain) AS number_of_jobs
+WHERE title NOT ILIKE '%Analyst%' AND title NOT ILIKE '%Analytics%'; 
+-- ans: 26 if case sensitive - analyst w/ all lowercase or all caps 
+-- ans: 4 if not case sensitive - data/tableau
+-- Bonus 
+SELECT domain, COUNT(domain) AS number_of_jobs
 FROM data_analyst_jobs
 WHERE domain IS NOT null
-AND skill LIKE '%SQL'
+AND skill ILIKE '%SQL%'
 AND days_since_posting > 21 
 GROUP BY domain
 ORDER BY number_of_jobs DESC; 
-Consulting (30), internet(19), health care(17), and finance(12) */
+-- ans: Internet (62), finance (61), consulting (57), health care (52)
